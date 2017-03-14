e7991e88df8b15ba50cf222f7c590e971bc7c269
taehoon <lth1011@gmail.com>
2017-03-15 00:05:11 +0900

    통합 코드 및 rmd 파일

--- overwatch_profile.R
+++ overwatch_profile.R
@@ -68,6 +68,73 @@ dbWriteTable(con, "user_compe_over", df_over, append=TRUE, row.names =F)
 # result <- dbGetQuery(con, query)
 # result
 
+#1. 경쟁전 랭킹 시각화 
+# 데이터 가져오기
+data_overall <- dbGetQuery(conn,"select * from user_compe_over")
+head(data_overall)
+
+# pal <- colorRamp(c("red","blue"))
+# palcol<-pal(seq(0,1,len=10))
+
+p<-ggplot(data=data_overall,aes(x=battletag,y=comprank,fill=battletag))+
+  geom_bar(stat="identity", position="identity", colour="white", width=0.5)+
+  ggtitle("경쟁전 랭킹")
+p+geom_text(aes(label=comprank))
+
+#경쟁전 주요 스탯 데이터 가져오기
+data_comp_avg <- dbGetQuery(conn,"select battletag 
+                            ,eliminations_avg
+                            ,damage_done_avg
+                            ,deaths_avg
+                            ,final_blows_avg
+                            ,healing_done_avg
+                            ,objective_kills_avg
+                            ,objective_time_avg
+                            ,solo_kills_avg
+                            from user_compe_avg")
+#kmeans clustering
+#처치 평균과 죽음 평균을 이용한 군집분석
+data1 <- subset(data_comp_avg,select=c(battletag,eliminations_avg,deaths_avg))
+
+data1_km <-kmeans(data1[,2:3],3)
+par(mfrow=c(1,1))
+plot(data1$deaths_avg,data1$eliminations_avg,type="n",xlab="죽음 평균",ylab="처치 평균")
+text(data1$deaths_avg,data1$eliminations_avg,labels=data1$battletag,cex=0.8,col=data1_km$cluster)
+points(data1_km$centers[,1:2], col="blue",pch=4,cex=2)
+
+#Hierarchical agglomerative clustering
+#데이터 표준화
+data_comp_avg_z <- sapply(data_comp_avg[,2:9],scale)
+data_comp_avg_z<-as.data.frame(data_comp_avg_z)
+data_comp_avg_z <- cbind(data_comp_avg$battletag,data_comp_avg_z)
+#outlier
+boxplot(data_comp_avg_z[2:9])
+#거리 구하기
+d <- dist(data_comp_avg_z[2:9])
+d
+hClustdat<-hclust(d,method="average")
+names(hClustdat)
+plot(hClustdat,hang=-1,cex=.8,main="Average Linkage Clustering",labels=data_comp_avg$battletag)
+rect.hclust(hClustdat, k = 5)
+install.packages("NbClust")
+library(NbClust)
+nc <- NbClust(data_comp_avg_z[,2:9],distance="euclidean",min.nc=2,max.nc=15,method="average")
+
+nc$Best.nc
+
+#공격부분 - 딜/ 처지 그래프
+# 데이터 추출
+data_avg_attack <- subset(data_comp_avg, select=c(battletag,eliminations_avg,damage_done_avg))
+
+g <- ggplot(data=data_avg_attack,aes(x=damage_done_avg,y=eliminations_avg,fill=battletag))
+g+geom_point(aes(color=battletag))+geom_text(aes(label=battletag,color=battletag),hjust=0,vjust=-1)
+
+#3. 수비부분 - 힐/ 죽음
+data_avg_defence <- subset(data_comp_avg, select=c(battletag,deaths_avg,healing_done_avg))
+g <- ggplot(data=data_avg_defence,aes(y=healing_done_avg,x=deaths_avg,fill=battletag))
+g+geom_point(aes(color=battletag))+geom_text(aes(label=battletag,color=battletag),hjust=0,vjust=0)
+
+#db disconnect
 dbDisconnect(con)
 
 
